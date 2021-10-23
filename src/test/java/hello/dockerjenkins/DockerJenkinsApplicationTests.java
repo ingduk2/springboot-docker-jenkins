fc687a59b0d600486de261d68d1ee479ee9ca09f
ingduk2 <ingduk2@naver.com>
2021-10-23 15:05:00 +0900

    Initial commit

---
+++ src/test/java/hello/dockerjenkins/DockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.dockerjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
